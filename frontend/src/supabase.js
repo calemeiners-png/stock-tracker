6285224e7536b6208c6d875b9600da4a9317a22f
Cale Meiners <calemeiners.com>
2026-05-27 02:09:13 -0500

    Fix Supabase credentials

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "your_project_url_here";
+const SUPABASE_URL = "https://nxdjqmwbjmcfgcwzztdz.supabase.co";
 const SUPABASE_ANON_KEY = "your_anon_key_here";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
